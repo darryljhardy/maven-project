5ba4006ef05faf847e8642531fd2e11d5ed3d0bc
darryljhardy <darryljhardy1@gmail.com>
2018-11-18 10:29:40 +0000

    Change test 1

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -19,7 +19,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "World";
+    String someone = "Darryl";
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
